309025e732ffcaefe5cb6fb797bb6f7cefe8f98b
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-10 02:17:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
   array.find {|words| words.length > 4} ? true : false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(the_food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  the_food.find {|cheese| cheese_types.include?(cheese)}
 end
