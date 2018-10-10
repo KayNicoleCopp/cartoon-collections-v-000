e80de429a0c0d3f690d110beac2186d3a4413c91
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-10 02:18:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 
 def find_the_cheese(the_food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  the_food.find {|cheese| cheese_types.include?(cheese)}
+  the_food.find {|cheese| cheese_types.include?(the_food)}
 end
